77150f71d030f25100941a963ac217eaa2b9c18e
avanvalk <drewvv@gmail.com>
2019-02-04 15:24:13 -0800

    scripts fixed, dependencies added, ensureUser created, app created

--- service/TransitApi.js
+++ service/TransitApi.js
@@ -1,14 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const request = require('superagent');
 
 const getRoute = (start, stop) => {
   return request
     .get(`https://transit.land/api/v1/stops/${name}`)
     .then(res => ({
-      start: res.body.stops.name;
+      start: res.body.stops.name
     }))
     .get(`https://transit.land/api/v1/stops/${name}`)
     .then(res => ({
-      stop: res.body.stops.name;
-    }))
+      stop: res.body.stops.name
+    }));
     
+};
+
+module.exports = { 
+  getRoute
 };
