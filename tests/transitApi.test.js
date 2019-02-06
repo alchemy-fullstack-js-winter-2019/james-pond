790f6488d6c5ee5a1497162f5e2ad175e850f561
avanvalk <drewvv@gmail.com>
2019-02-06 10:55:10 -0800

    working on transitApi

--- tests/transitApi.test.js
+++ tests/transitApi.test.js
@@ -2,14 +2,20 @@ const {
   getRoute
 } = require('../service/TransitApi');
 
-describe.skip('transit api service', () => {
-  it('gets a stop by name', () => {
-    return getRoute(1)
-      .then(stop => {
-        expect(stop).toEqual({
-          name: 'SW 5th & Alder',
+describe('transit api service', () => {
+  // it('gets a stop by name', () => {
+  //   return getRoute()
+  //     .then(stop => {
+  //       expect(stop).toEqual({
+  //         name: 'SW 5th & Alder',
 
-        });
+  //       });
+  //     });
+  // });
+  it('gets a list of stops', () => {
+    return getRoutes()
+      .then(routes => {
+        expect(routes).toHaveLength(20);
       });
   });
 });
