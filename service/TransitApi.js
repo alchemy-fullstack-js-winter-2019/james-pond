caaeea630e55c1ca271534be1f1c1f52de980d42
Connor Frendt <cjacksonfx@yahoo.com>
2019-02-06 11:40:46 -0800

    can get all stops in portland with a radius of 2 miles from specified location

--- service/TransitApi.js
+++ service/TransitApi.js
@@ -4,9 +4,9 @@ const request = require('superagent');
 
 const getRoutes = (num) => {
   return request
-    .get('https://transit.land/api/v1/stops/')
+    .get('https://transit.land/api/v1/stops?lat=45.51925&lon=-122.68197&r=3200')
     .then(res => {
-      console.log('res here', res.body.stops[0].name);
+      console.log('res here', res.body.stops);
       // start: res.body.stops
     });
     
