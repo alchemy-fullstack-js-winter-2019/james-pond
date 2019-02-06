ba0f0767792fccee529003ae35f36c0bafbba45d
Connor Frendt <cjacksonfx@yahoo.com>
2019-02-06 11:45:52 -0800

    prints list of 50 stops in pdx

--- service/TransitApi.js
+++ service/TransitApi.js
@@ -6,7 +6,10 @@ const getRoutes = (num) => {
   return request
     .get('https://transit.land/api/v1/stops?lat=45.51925&lon=-122.68197&r=3200')
     .then(res => {
-      console.log('res here', res.body.stops);
+      for(let i = 0; i < res.body.stops.length; i++) {
+        console.log('res here', res.body.stops[i].name);
+
+      }
       // start: res.body.stops
     });
     
