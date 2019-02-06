91c3fea028cb931b9d408a22ffe6841a6b66e6d8
Abel Quintero <abel.j.quintero@gmail.com>
2019-02-06 13:13:45 -0800

    prints list of stops test passing

--- tests/transitApi.test.js
+++ tests/transitApi.test.js
@@ -3,20 +3,14 @@ const {
 } = require('../service/TransitApi');
 
 describe('transit api service', () => {
-  // it('gets a stop by name', () => {
-  //   return getRoute()
-  //     .then(stop => {
-  //       expect(stop).toEqual({
-  //         name: 'SW 5th & Alder',
 
-  //       });
-  //     });
-  // });
   it('gets a list of stops', () => {
     return getRoutes(20)
       .then(routes => {
-        console.log(routes);
-        expect(routes).toHaveLength(20);
+        console.log('list of routes', routes);
+        expect(routes).toHaveLength(50);
       });
   });
+
+
 });
