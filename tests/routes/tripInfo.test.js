6577bc3885ed5c8937c514548d3165c08a906c0a
Abel Quintero <abel.j.quintero@gmail.com>
2019-02-06 16:13:41 -0800

    getFullTripInfo test failing as expected

--- tests/routes/tripInfo.test.js
+++ tests/routes/tripInfo.test.js
@@ -1,21 +1,37 @@
-const { getComment, getToken } = require('./dataHelpers');
+require('dotenv').config();
+// const { getComment, getToken } = require('../dataHelpers');
 const request = require('supertest');
 const { Types } = require('mongoose');
+const connect = require('../../lib/utils/connect');
+const mongoose = require('mongoose');
 
-const app = require('../lib/app');
+const app = require('../../lib/app');
 
 describe('tripInfoRoute', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
   it('can get fullTripInfo', () => {
     return request(app)
-      .get('/auth/tripInfo')
-      .send({ stopName, coordinates, comments })
+      .get('/tripInfo')
+      .send({ stopName: 'SW 5th & Alder', coordinates: [100, 120], comments: ['hi', 'holA'] })
       .then(res => {
+        console.log('body here', res.body);
         expect(res.body).toEqual({
           _id: expect.any(Types.ObjectId),
           stopName: expect.any(String),
           coordinates: expect.any(Array),
           comments: expect.any(Array)
-        })
-      })
-  })
-};
+        });
+      });
+  });
+});
