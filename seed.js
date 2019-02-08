d4334b01b048832bf4b1768dee16b687c5c6cb58
Connor Frendt <cjacksonfx@yahoo.com>
2019-02-08 10:51:23 -0800

    got seed data to show in robo3t with 10 users and 50 comments!!!!!!!

--- seed.js
+++ seed.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 require('dotenv').config();
-require('./lib/utils/connect');
+require('./lib/utils/connect')();
 const mongoose = require('mongoose');
 const seedData = require('./tests/seedData');
-
+console.log('RUNNING SEED');
 seedData({})
   .then(() => console.log('done'))
   .finally(() => mongoose.connection.close());
